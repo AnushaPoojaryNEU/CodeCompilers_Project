c1eff7faa91a85f710f0e9bd765faaefebb7eac4
AnushaPoojaryNEU <poojary.an@northeastern.edu>
2021-12-01 18:34:03 -0500

    Organization,UserAcc,Role changes

--- HomelessSupportSystem/src/business/EcoSystem.java
+++ HomelessSupportSystem/src/business/EcoSystem.java
@@ -4,10 +4,33 @@
  */
 package business;
 
+import business.organization.Organization;
+import business.network.Network;
+import business.useraccount.UserAccountDirectory;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author anu61
  */
-public class EcoSystem {
+public class EcoSystem extends Organization {
     
+   private static EcoSystem ecoSystem; 
+   private UserAccountDirectory userAccountDirectory;
+     
+   private List<Network> networkList;
+   
+   public EcoSystem() {
+        super(null);
+        this.userAccountDirectory = userAccountDirectory;
+        this.networkList = new ArrayList();
+   }
+   
+    public static EcoSystem getInstance() {
+        if (ecoSystem == null) {
+            ecoSystem = new EcoSystem();
+        }
+        return ecoSystem;
+    }
 }
