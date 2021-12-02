df9f6d682cc08323c78745b8a59ab4e0539fbab8
Dwithika12 <shetty.dw@northeastern.edu>
2021-12-01 22:48:31 -0500

    individual

---
+++ HomelessSupportSystem/src/business/individuals/Individual.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.individuals;
+
+import java.util.List;
+
+/**
+ *
+ * @author dwith
+ */
+public class Individual {
+    
+    private int registrationId;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String birthDate;
+    private String address;
+    private String city;
+    private int zipCode;
+    private IDD iddInfo;
+    private String serviceType;
+    private List<IndividualHistory> history;
+}
--- HomelessSupportSystem/src/business/role/Role.java
+++ HomelessSupportSystem/src/business/role/Role.java
@@ -4,10 +4,43 @@
  */
 package business.role;
 
+import business.EcoSystem;
+
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author anu61
  */
-public class Role {
+public abstract class Role {
     
+    public enum RoleType{
+        Admin("Admin"),
+        Doctor("Doctor"),
+        Nurse("Nurse"),
+        Caregiver("Caregiver"),
+        Reviewer("Reviewer"),
+        Individual("Individual"),
+        SystemAdmin("SystemAdmin"),
+        Trainer("Trainer");
+        
+        
+        private final String value;
+        private RoleType(String value){
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return this.getValue();
+        }
+    }
+    
+    public abstract JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business, Enterprise enterprise,Individual individual);
+
 }
