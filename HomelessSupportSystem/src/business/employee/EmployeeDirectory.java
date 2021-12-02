ceac93fd05bc45c32af8156e5f41213d373d9dcd
AnushaPoojaryNEU <poojary.an@northeastern.edu>
2021-12-01 22:44:11 -0500

    Added Organization classes

--- HomelessSupportSystem/src/business/employee/EmployeeDirectory.java
+++ HomelessSupportSystem/src/business/employee/EmployeeDirectory.java
@@ -4,10 +4,37 @@
  */
 package business.employee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author dwith
  */
 public class EmployeeDirectory {
+    
+    public List<Employee> employeeList;
+
+    public EmployeeDirectory() {
+        this.employeeList = new ArrayList();
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public Employee createEmployee(String Name) {
+        Employee emp = new Employee(Name);
+        employeeList.add(emp);
+        return emp;
+    }
+    
+    public void deleteEmployee(Employee employee) {
+        employeeList.remove(employee);
+    }
     
 }
