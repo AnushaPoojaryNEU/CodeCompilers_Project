485c7f435ca2ba7af37ce7e7d66aabe793c380e8
Dwithika12 <shetty.dw@northeastern.edu>
2021-12-04 02:23:48 -0500

    individualm

--- HomelessSupportSystem/src/business/network/Network.java
+++ HomelessSupportSystem/src/business/network/Network.java
@@ -4,10 +4,35 @@
  */
 package business.network;
 
+import business.enterprise.EnterpriseDirectory;
+
 /**
  *
  * @author anu61
  */
 public class Network {
     
+    private String name;
+    private final EnterpriseDirectory enterpriseDirectory;
+        
+    public Network() {
+        this.enterpriseDirectory = new EnterpriseDirectory();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
 }
