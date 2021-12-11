dafb74ac50219b5f6cb7d6318c79739c203f7460
jagadishwararajug <90645217+jagadishwararajug@users.noreply.github.com>
2021-12-11 17:22:35 -0500

    Create MedicalOrganization.java

---
+++ HomelessSupportSystem/src/business/organization/MedicalOrganization.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.organization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Geetha
+ */
+public class MedicalOrganization extends Organization{
+
+    public MedicalOrganization(String name) {
+        super(name);
+    }
+
+   
+    
+}
