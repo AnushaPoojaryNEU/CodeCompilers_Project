93132abda77e4bb306294fb46f19ac7f8fbe7dc1
jagadishwararajug <90645217+jagadishwararajug@users.noreply.github.com>
2021-12-11 17:40:18 -0500

    Added a utility function to parse role from string

--- HomelessSupportSystem/src/business/useraccount/UserAccount.java
+++ HomelessSupportSystem/src/business/useraccount/UserAccount.java
@@ -99,6 +99,40 @@ public class UserAccount {
     }
 
 
-   
+    private Role parseRoleFromString(String role) {
+        try {
+            RoleType rt = RoleType.valueOf(role); 
+            if (rt.equals(RoleType.CHEF)) 
+                return new Chef();
+            else if (rt.equals(RoleType.DELIVERY_MAN))
+                return new DeliveryMan();
+            else if (rt.equals(RoleType.DOCTOR))
+                return new Doctor();
+            else if (rt.equals(RoleType.ENTERPRISE_ADMIN))
+                return new AdminRole();
+            else if (rt.equals(RoleType.FOOD_SUPPLY_MANAGER))
+                return new FoodSupplyManager();
+            else if (rt.equals(RoleType.HOMELESS_INDIVIDUAL))
+                return new HomelessIndividual();
+            else if (rt.equals(RoleType.HOUSING_MANAGER))
+                return new HousingManager();
+            else if (rt.equals(RoleType.INSTRUCTOR))
+                return new Instructor();
+            else if (rt.equals(RoleType.MAINTENANCE_CREW_MEMBER))
+                return new MaintenanceCrewMember();
+            else if (rt.equals(RoleType.NURSE))
+                return new Nurse();
+            else if (rt.equals(RoleType.PROGRAM_MANAGER))
+                return new TrainingProgramManager();
+            else if (rt.equals(RoleType.RECEPTIONIST))
+                return new Receptionist();
+            else if (rt.equals(RoleType.SYS_ADMIN)) 
+                return new SystemAdminRole();
+            else
+                return null;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
     
 }
