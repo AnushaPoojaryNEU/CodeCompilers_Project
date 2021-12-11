ebb78e8b44cb39a948f2392527172065e4fe57d6
jagadishwararajug <90645217+jagadishwararajug@users.noreply.github.com>
2021-12-11 17:21:21 -0500

    Create HouseMaintenanceOrganization.java

---
+++ HomelessSupportSystem/src/business/organization/HouseMaintenanceOrganization.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.organization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Geetha
+ */
+public class HouseMaintenanceOrganization extends Organization{
+    
+    public HouseMaintenanceOrganization(String name) {
+        super(name);
+    }
+
+   
+}
