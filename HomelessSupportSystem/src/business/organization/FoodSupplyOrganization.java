b373c390d5cf9ed910e8e2c87908d25a0161d9a6
jagadishwararajug <90645217+jagadishwararajug@users.noreply.github.com>
2021-12-11 14:49:24 -0500

    Create FoodSupplyOrganization.java

---
+++ HomelessSupportSystem/src/business/organization/FoodSupplyOrganization.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.organization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Geetha
+ */
+public class FoodSupplyOrganization extends Organization {
+    public FoodSupplyOrganization(String name) {
+        super(name);
+    }
+
+    
+}
