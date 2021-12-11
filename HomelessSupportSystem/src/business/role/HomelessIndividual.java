875e71d68eed2e0429d24b4422f255c16360fb07
jagadishwararajug <90645217+jagadishwararajug@users.noreply.github.com>
2021-12-11 14:21:04 -0500

    Create HomelessIndividual.java

---
+++ HomelessSupportSystem/src/business/role/HomelessIndividual.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.role;
+
+import business.EcoSystem;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+import ui.sheltermonitoring.HomelessIndividualJPanel;
+
+/**
+ *
+ * @author Geetha
+ */
+public class HomelessIndividual extends Role {
+
+    
+    
+}
